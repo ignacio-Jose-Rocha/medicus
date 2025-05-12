5b722f4eeeae8e0c73f40421bb6c76639cf637d4
ignacio rocha <ignac1997nacho@gmail.com>
2025-05-12 15:41:36 -0300

    a

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
   build: {
     outDir: 'dist',
   },
-  // 👇 esta parte es la importante
-  resolve: {
-    alias: {
-      // tus alias si tenés
-    },
-  },
-  // 👇 esto es lo que permite rutas con React Router en producción
   base: '/',
 });
 
